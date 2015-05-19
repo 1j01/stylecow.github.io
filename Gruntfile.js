ce49edb623220feeecca101e1222af6d5597126d
oscarotero <oom@oscarotero.com>
2015-05-19 20:46:54 +0200

    updated demo to the latest version

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,22 +9,7 @@ module.exports = function(grunt) {
 			options: {
 				require: [
 					'./node_modules/codemirror/lib/codemirror:codemirror',
-					'./node_modules/stylecow:stylecow',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-calc:stylecow-plugin-calc',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-color:stylecow-plugin-color',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-custom-media:stylecow-plugin-custom-media',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-custom-selector:stylecow-plugin-custom-selector',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-extend:stylecow-plugin-extend',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-fixes:stylecow-plugin-fixes',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-flex:stylecow-plugin-flex',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-matches:stylecow-plugin-matches',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-msfilter-background-alpha:stylecow-plugin-msfilter-background-alpha',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-msfilter-linear-gradient:stylecow-plugin-msfilter-linear-gradient',
-					//'./node_modules/stylecow/node_modules/stylecow-plugin-msfilter-transform:stylecow-plugin-msfilter-transform',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-nested-rules:stylecow-plugin-nested-rules',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-prefixes:stylecow-plugin-prefixes',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-rem:stylecow-plugin-rem',
-					'./node_modules/stylecow/node_modules/stylecow-plugin-variables:stylecow-plugin-variables',
+					'./node_modules/stylecow:stylecow'
 				]
 			}
 		},
--- js/bundler.js
+++ js/bundler.js
@@ -1,29 +1,32 @@
 var stylecow = require('stylecow');
+
 require('codemirror');
+
+//require all dinamic modules for browserify
 require('../node_modules/codemirror/mode/css/css');
 
-stylecow
-	.loadPlugin('calc')
-	.loadPlugin('color')
-	.loadPlugin('custom-media')
-	.loadPlugin('custom-selector')
-	.loadPlugin('extend')
-	.loadPlugin('fixes')
-	.loadPlugin('flex')
-	.loadPlugin('matches')
-	.loadPlugin('msfilter-background-alpha')
-	.loadPlugin('msfilter-linear-gradient')
-	//.loadPlugin('msfilter-transform')
-	.loadPlugin('nested-rules')
-	.loadPlugin('prefixes')
-	.loadPlugin('rem')
-	.loadPlugin('variables')
-	.minSupport({
-		explorer: false,
-		firefox: false,
-		chrome: false,
-		safari: false,
-		opera: false,
-		android: false,
-		ios: false
-	});
+require('../node_modules/stylecow/node_modules/stylecow-plugin-calc')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-color')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-custom-media')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-custom-selector')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-extend')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-fixes')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-flex')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-matches')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-msfilter-background-alpha')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-msfilter-linear-gradient')(stylecow);
+//'./node_modules/stylecow/node_modules/stylecow-plugin-msfilter-transform',
+require('../node_modules/stylecow/node_modules/stylecow-plugin-nested-rules')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-prefixes')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-rem')(stylecow);
+require('../node_modules/stylecow/node_modules/stylecow-plugin-variables')(stylecow);
+
+stylecow.minSupport({
+	explorer: false,
+	firefox: false,
+	chrome: false,
+	safari: false,
+	opera: false,
+	android: false,
+	ios: false
+});
